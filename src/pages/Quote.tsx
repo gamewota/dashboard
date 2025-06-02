c2ba5f09bf7965a26a08229f335a842b561c9f84
Moh Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2025-06-02 21:04:48 +0700

    feat: integrate quotes page to backend

--- src/pages/Quote.tsx
+++ src/pages/Quote.tsx
@@ -1,6 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchQuotes } from '../features/quotes/quoteSlice';
+import { useEffect } from 'react';
+import type { RootState, AppDispatch } from '../store';
+
 const Quote = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const {data, loading, error} = useSelector((state: RootState) => state.quotes);
+
+  useEffect(() => {
+    dispatch(fetchQuotes());
+  },[dispatch]);
+
   return (
-    <div className='min-h-screen w-screen flex items-center justify-center'>Quote</div>
+    <div className='min-h-screen w-screen flex justify-center'>
+      {loading && <p>Loading...</p>}
+      {error && <p className='text-bold'>Error: {error}</p>}
+
+      <div className='overflow-x-auto'>
+        <table className='table table-zebra'>
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>Quote Text</td>
+            </tr>
+          </thead>
+          <tbody>
+            {data && data.map((quote,index) => {
+              return (
+                <tr key={quote.id}>
+                  <td>{index + 1}</td>
+                  <td>{quote.text}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from '../features/users/userSlice';
 import songsReducer from '../features/songs/songSlice';
+import quotesReducer from '../features/quotes/quoteSlice';
 
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
-        songs: songsReducer
+        songs: songsReducer,
+        quotes: quotesReducer
     }
 })
 
