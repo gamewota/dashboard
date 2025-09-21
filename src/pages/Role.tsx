79bd54ff967025c2a937d7ccdc2db5ead558a412
Moh Alief Rizky Ramadhan <alief.ramadhan@pasarpolis.com>
2025-09-21 14:52:53 +0700

    feat: add functionality for update permission on certain roles

--- src/pages/Role.tsx
+++ src/pages/Role.tsx
@@ -1,24 +1,62 @@
 import { useSelector, useDispatch } from 'react-redux';
 import type { RootState, AppDispatch } from '../store';
-import { fetchRoles } from '../features/roles/roleSlice';
+import { fetchRoles, assignPermissionToRole, removePermissionFromRole } from '../features/roles/roleSlice';
+import { fetchPermissions } from "../features/permissions/permissionsSlice";
 import { useEffect } from 'react';
 import { DataTable } from '../components/DataTable';
+import { useToast } from "../hooks/useToast";
+import MultiSelect from '../components/MultiSelect';
 
 
 const Role = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const { showToast, ToastContainer } = useToast();
   const {data, loading, error} = useSelector((state: RootState) => state.roles);
+  const {data: permissionsData} = useSelector((state: RootState) => state.permissions)
+
 
   const columns = 
     [
       { header: '#', accessor: (_row: any, i: number) => i + 1 as React.ReactNode },
       { header: 'Role Name', accessor: (row: { name: string; description: string }) => row.name },
       { header: 'Description', accessor: (row: { name: string; description: string }) => row.description },
+      {header: 'Permissions', accessor: (row: any) => (
+        <MultiSelect 
+          options={permissionsData}
+          initialSelected={row.permissions.map((r: any) => r.id)}
+          onAdd={(permissionId) => handleAddPermission(row.id, permissionId)}
+          onRemove={(permissionId) => handleRemovePermission(row.id, permissionId)}
+          onSuccess={(msg) => showToast(msg || "Permission removed successfully ✅", "success")}
+          onFailure={(err : any) => showToast(err || "Failed to remove permission ❌", "error")}
+        />
+      )}
     ]
 
   useEffect(() => {
       dispatch(fetchRoles());
+      dispatch(fetchPermissions());
     }, [dispatch]);
+
+
+  const handleAddPermission = async (roleId: number, permissionId: number) => {
+    try {
+      await dispatch(assignPermissionToRole({roleId, permissionId})).unwrap();
+      dispatch(fetchRoles())
+    } catch (err : any) {
+      showToast(err || "Failed to assign permission ❌", "error");
+    }
+  }
+
+  const handleRemovePermission = async (roleId: number, permissionId: number) => {
+    try {
+      await dispatch(removePermissionFromRole({roleId, permissionId})).unwrap();
+      dispatch(fetchRoles())
+    } catch (err: any) {
+      showToast(err || "Failed to remove permission ❌", "error");
+    }
+  }
+
+  
 
   if (loading) return (
     <div className='min-h-screen w-screen flex justify-center items-center'>
@@ -51,6 +89,7 @@ const Role = () => {
           emptyMessage={'No roles found.'}
         />
       </div>
+      <ToastContainer />
     </div>
   )
 }
